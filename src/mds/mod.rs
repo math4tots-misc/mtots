335c4e59652e921768ad6ec5bd538a7f68e4170d
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-12 12:34:38 -0700

    move standard library modules to mtots_core

--- src/mds/mod.rs
+++ src/mds/mod.rs
@@ -9,16 +9,12 @@ use std::rc::Rc;
 
 mod ggez;
 mod json;
-mod math;
-mod mfs;
 mod three;
 
 pub fn add_standard_modules(globals: &mut Globals) {
     add(globals, json::NAME, json::load);
-    add(globals, mfs::NAME, mfs::load);
     add(globals, ggez::NAME, ggez::load);
     add(globals, three::NAME, three::load);
-    add(globals, math::NAME, math::load);
 }
 
 fn add<F>(globals: &mut Globals, name: &'static str, body: F)
