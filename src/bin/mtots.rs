96a4de2b155e20fe0f174f9edd48779ebf80af77
Kyumin Kim <math4tots@users.noreply.github.com>
2020-06-18 23:42:18 -0700

    Update to work with trampoline support

--- src/bin/mtots.rs
+++ src/bin/mtots.rs
@@ -3,5 +3,5 @@ extern crate mtots;
 fn main() {
     let mut globals = mtots::Globals::new();
     mtots::add_standard_modules(&mut globals);
-    mtots::main(&mut globals);
+    mtots::main(globals);
 }
