abdc9b67fd69c6662d341c9c3ae02699807b897b
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-09 15:47:51 -0700

    Disable all modules for now, to make build pass

--- src/mds/mod.rs
+++ src/mds/mod.rs
@@ -1,27 +1,14 @@
 use crate::Globals;
 
-use crate::EvalResult;
-use crate::HMap;
-use crate::RcStr;
-use crate::Value;
-use std::cell::RefCell;
-use std::rc::Rc;
+// mod ggez;
+// mod json;
+// mod rand;
+// mod regex;
 
-mod ggez;
-mod json;
-mod rand;
-mod regex;
-
-pub fn add_standard_modules(globals: &mut Globals) {
-    add(globals, json::NAME, json::load);
-    add(globals, rand::NAME, rand::load);
-    add(globals, regex::NAME, regex::load);
-    add(globals, ggez::NAME, ggez::load);
+pub fn add_standard_modules(_globals: &mut Globals) {
+    // add(globals, json::NAME, json::load);
+    // add(globals, rand::NAME, rand::load);
+    // add(globals, regex::NAME, regex::load);
+    // add(globals, ggez::NAME, ggez::load);
 }
 
-fn add<F>(globals: &mut Globals, name: &'static str, body: F)
-where
-    F: FnOnce(&mut Globals) -> EvalResult<HMap<RcStr, Rc<RefCell<Value>>>> + 'static,
-{
-    globals.add_native_module(name.into(), body);
-}
