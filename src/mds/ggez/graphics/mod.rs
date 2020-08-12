ae69d90fee6b651c251689b0f3084a8657577aeb
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-11 18:18:41 -0700

    ggez: queue_text/draw_queued_text

--- src/mds/ggez/graphics/mod.rs
+++ src/mds/ggez/graphics/mod.rs
@@ -33,6 +33,48 @@ pub(in super::super) fn new() -> NativeModule {
                 Ok(Value::Nil)
             },
         );
+        m.func(
+            "queue_text",
+            ArgSpec::builder().req("text").def("x", 0).def("y", 0).def("color", ()),
+            "",
+            |globals, args, _| {
+                let mut args = args.into_iter();
+                let textval = args.next().unwrap();
+                let text = textval.to_xref::<Text>(globals)?;
+                let x = f32::try_from(args.next().unwrap())?;
+                let y = f32::try_from(args.next().unwrap())?;
+                let color: Option<ggez::graphics::Color> = match args.next().unwrap() {
+                    Value::Nil => None,
+                    value => Some(Color::try_from(value)?.into()),
+                };
+                let ctx = getctx(globals)?;
+                ggez::graphics::queue_text(
+                    ctx,
+                    text.get(),
+                    [x, y],
+                    color,
+                );
+                Ok(Value::Nil)
+            },
+        );
+        m.func(
+            "draw_queued_text",
+            ArgSpec::builder().def("x", 0).def("y", 0),
+            "",
+            |globals, args, _| {
+                let mut args = args.into_iter();
+                let x = f32::try_from(args.next().unwrap())?;
+                let y = f32::try_from(args.next().unwrap())?;
+                let ctx = getctx(globals)?;
+                mtry!(ggez::graphics::draw_queued_text(
+                    ctx,
+                    DrawParam::default().dest([x, y]),
+                    None,
+                    ggez::graphics::FilterMode::Linear,
+                ));
+                Ok(Value::Nil)
+            },
+        );
         m.class::<Text, _>("Text", |cls| {
             cls.sfunc("__call", ["arg"], "", |globals, args, _| {
                 let mut args = args.into_iter();
