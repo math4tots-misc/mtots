50ccaf93efc6efe548d61828c0a00ecc237373e8
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-14 18:05:02 -0700

    basekit feature

--- src/mds/mod.rs
+++ src/mds/mod.rs
@@ -2,14 +2,23 @@ use crate::Globals;
 
 #[cfg(feature = "gamekit")]
 mod ggez;
+
+#[cfg(feature = "basekit")]
 mod json;
+
+#[cfg(feature = "basekit")]
 mod rand;
+
+#[cfg(feature = "basekit")]
 mod regex;
 
 pub fn add_standard_modules(globals: &mut Globals) {
-    globals.add(json::new()).unwrap();
-    globals.add(rand::new()).unwrap();
-    globals.add(regex::new()).unwrap();
+    #[cfg(feature = "basekit")]
+    {
+        globals.add(json::new()).unwrap();
+        globals.add(rand::new()).unwrap();
+        globals.add(regex::new()).unwrap();
+    }
 
     #[cfg(feature = "gamekit")]
     {
