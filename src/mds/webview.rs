28dc08a37066332eee90d91ed131578c161b1e85
Kyumin Kim <math4tots@users.noreply.github.com>
2020-08-18 01:27:07 -0700

    canvas stuff WIP

--- src/mds/webview.rs
+++ src/mds/webview.rs
@@ -2,8 +2,12 @@
 use crate::mtry;
 use crate::ordie;
 use crate::ArgSpec;
+use crate::Globals;
 use crate::NativeModule;
+use crate::Promise;
+use crate::Result;
 use crate::Value;
+use std::collections::HashMap;
 use std::convert::TryFrom;
 
 pub const NAME: &str = "a.webview";
@@ -36,12 +40,25 @@ pub(super) fn new() -> NativeModule {
                     .debug(debug)
                     .user_data(())
                     .invoke_handler(|webview, arg| {
-                        let webview = WV(unsafe { std::mem::transmute(webview) });
-                        let r = globals.new_handle::<WV>(webview);
-                        let webview = Value::from(ordie(globals, r));
-                        let arg = Value::from(arg);
-                        let r = handler.apply(globals, vec![webview, arg], None);
-                        ordie(globals, r);
+                        if arg.starts_with("eval/") {
+                            println!("EVAL: {}", arg);
+                            let mut iter = arg.splitn(3, '/');
+                            iter.next().unwrap(); // eval
+                            let id: usize = iter.next().unwrap().parse().unwrap();
+                            let resolve = {
+                                let mut reg =
+                                    globals.stash_mut().get_mut::<JsRequestRegistry>().unwrap();
+                                reg.resolve_map.remove(&id).unwrap()
+                            };
+                            resolve(globals, Ok(Value::from(iter.next().unwrap())));
+                        } else {
+                            let webview = WV(unsafe { std::mem::transmute(webview) });
+                            let r = globals.new_handle::<WV>(webview);
+                            let webview = Value::from(ordie(globals, r));
+                            let arg = Value::from(arg);
+                            let r = handler.apply(globals, vec![webview, arg], None);
+                            ordie(globals, r);
+                        }
                         Ok(())
                     })
                     .run();
@@ -57,6 +74,40 @@ pub(super) fn new() -> NativeModule {
                 mtry!(r);
                 Ok(().into())
             });
+            cls.ifunc(
+                "async_eval",
+                ["js"],
+                concat!(
+                    "Evaluates a js code snippet, and (asynchronously) ",
+                    "returns the result (as a string)",
+                ),
+                |owner, globals, args, _| {
+                    println!("start ASYNC_EVAL");
+                    let mut args = args.into_iter();
+                    let js = args.next().unwrap().into_string()?;
+                    let id = {
+                        let mut reg = globals.stash_mut().get_mut::<JsRequestRegistry>()?;
+                        reg.next_id += 1;
+                        reg.next_id
+                    };
+                    let promise = Promise::new(globals, |globals, resolve| {
+                        let mut reg = globals.stash_mut().get_mut::<JsRequestRegistry>().unwrap();
+                        reg.resolve_map.insert(id, resolve);
+                    });
+                    println!("EVAL START: {}", format!(
+                        "external.invoke('eval/{}/' + {})",
+                        id,
+                        js.str(),
+                    ));
+                    let r = owner.borrow_mut().0.eval(&format!(
+                        "external.invoke('eval/{}/' + {})",
+                        id,
+                        js.str(),
+                    ));
+                    mtry!(r);
+                    Ok(promise.into())
+                },
+            );
             cls.ifunc("set_title", ["title"], "", |owner, _globals, args, _| {
                 let mut args = args.into_iter();
                 let title = args.next().unwrap().into_string()?;
@@ -64,15 +115,39 @@ pub(super) fn new() -> NativeModule {
                 mtry!(r);
                 Ok(().into())
             });
-            cls.ifunc("set_fullscreen", ["title"], "", |owner, _globals, args, _| {
-                let mut args = args.into_iter();
-                let fullscreen = args.next().unwrap().truthy();
-                owner.borrow_mut().0.set_fullscreen(fullscreen);
-                Ok(().into())
-            });
+            cls.ifunc(
+                "set_fullscreen",
+                ["title"],
+                "",
+                |owner, _globals, args, _| {
+                    let mut args = args.into_iter();
+                    let fullscreen = args.next().unwrap().truthy();
+                    owner.borrow_mut().0.set_fullscreen(fullscreen);
+                    Ok(().into())
+                },
+            );
+            cls.ifunc(
+                "exit",
+                (),
+                "",
+                |owner, _globals, _args, _| {
+                    owner.borrow_mut().0.exit();
+                    Ok(().into())
+                },
+            );
+        });
+        m.action(|globals, _map| {
+            globals.stash_mut().set(JsRequestRegistry::default())?;
+            Ok(())
         });
     })
 }
 
 /// wrapper around WebView, so that they can be used in mtots
 struct WV(&'static mut web_view::WebView<'static, ()>);
+
+#[derive(Default)]
+struct JsRequestRegistry {
+    next_id: usize,
+    resolve_map: HashMap<usize, Box<dyn FnOnce(&mut Globals, Result<Value>)>>,
+}
